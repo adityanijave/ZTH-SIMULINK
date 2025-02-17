3aa4f31de0ed5cfc315d53664524c96d17d27cf2
AdityaNijave <adityanijave09@gmail.com>
2025-02-17 18:52:02 +0530

    changes in m script and made damper constant 0 to oberse the effect

---
+++ Task/task7_damperEqu/damperEquMfile_b.m
@@ -0,0 +1,10 @@
+
+
+M = 1;                                                                      %in kg
+F = 1;                                                                      %N/m;
+k = 20; 
+b = 0;                                                                      %no damper
+
+
+sim("task7b.slx");
+disp("program exe.");
